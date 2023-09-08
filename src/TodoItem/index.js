c0139c60bb2a5cd312d25e60788062c0445528b0
wilgar <wilenri341@hotmail.com>
2023-09-08 14:50:52 -0500

    Actualizacion de estilos

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -4,20 +4,22 @@ import './TodoItem.css';
 
 function TodoItem(props) {
   return (
-    <li className="TodoItem">
-      <CompleteIcon
-        completed={props.completed}
-        onComplete={props.onComplete}
-      />
+   <div className='TodoItemContainer'>
+      <li className="TodoItem">
+        <CompleteIcon
+          completed={props.completed}
+          onComplete={props.onComplete}
+        />
 
-      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
-        {props.text}
-      </p>
+        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+          {props.text}
+        </p>
 
-      <DeleteIcon
-        onDelete={props.onDelete}
-      />
-    </li>
+        <DeleteIcon
+          onDelete={props.onDelete}
+        />
+      </li>
+    </div> 
   );
 }
 
